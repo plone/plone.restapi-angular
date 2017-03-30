8d0835654bd032e5c00c020f6a640555fcba69d4
Eric BREHAULT <ebrehault@gmail.com>
2017-03-30 14:23:59 +0200

    process image and file urls in rich text returned by backend

--- src/views/view.ts
+++ src/views/view.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Traverser } from 'angular-traversal';
+import { ConfigurationService } from '../configuration.service';
 
 @Component({
   selector: 'plone-view',
@@ -10,13 +11,19 @@ export class ViewView implements OnInit {
   private context: any;
   private text: string;
 
-  constructor(private traverser: Traverser) { }
+  constructor(
+    private config: ConfigurationService,
+    private traverser: Traverser,
+  ) { }
 
   ngOnInit() {
     this.traverser.target.subscribe(target => {
       this.context = target.context;
-      if(target.context.text) {
-        this.text = target.context.text.data;
+      if (target.context.text) {
+        // NEEDED UNTIL PLONE.RESTAPI RETURNS FULL PATHES
+        this.text = target.context.text.data.replace(
+          /(src|href)=".+(resolveuid\/.+?)"/g,
+          'src="' + this.config.get('BACKEND_URL') + '/$2"');
       }
     });
   }
