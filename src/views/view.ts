2288a9319d36ce2d9e96bd969ef687d9efb5a6cd
Eric BREHAULT <ebrehault@gmail.com>
2017-05-24 13:25:56 +0200

    expose title and description meta in head

--- src/views/view.ts
+++ src/views/view.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+
 import { Traverser } from 'angular-traversal';
 import { ConfigurationService } from '../configuration.service';
 import { TraversingComponent } from '../traversing';
@@ -14,11 +16,18 @@ export class ViewView extends TraversingComponent {
   constructor(
     private config: ConfigurationService,
     private traverser: Traverser,
+    private meta: Meta,
+    private title: Title,
   ) {
     super(traverser);
   }
 
   onTraverse(target) {
+    this.title.setTitle(target.context.title);
+    this.meta.updateTag({
+      name: 'description',
+      content: target.context.description
+    });
     if (target.context.text) {
       // NEEDED UNTIL PLONE.RESTAPI RETURNS FULL PATHS
       this.text = target.context.text.data.replace(
