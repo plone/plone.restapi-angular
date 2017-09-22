ab078defd32884cdd78b8d534b31b02d117ad2ea
Fulvio Casali <fulviocasali@gmail.com>
2017-09-22 16:24:15 +0200

    Convert comments.service tests from @angular/http to @angular/common/http.

--- src/comments.service.spec.ts
+++ src/comments.service.spec.ts
@@ -2,12 +2,9 @@
 
 import { TestBed, inject } from '@angular/core/testing';
 import {
-  BaseRequestOptions,
-  Response,
-  ResponseOptions,
-  Http
-} from '@angular/http';
-
+  HttpTestingController,
+  HttpClientTestingModule
+} from '@angular/common/http/testing';
 import {
   MockBackend,
   MockConnection
@@ -21,6 +18,9 @@ import { CommentsService } from './comments.service';
 describe('CommentsService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
       providers: [
         CommentsService,
         APIService,
@@ -31,67 +31,66 @@ describe('CommentsService', () => {
             BACKEND_URL: 'http://fake/Plone',
           }
         },
-        BaseRequestOptions,
-        MockBackend,
-        {
-          provide: Http,
-          useFactory: (backend: MockBackend, defaultOptions: BaseRequestOptions) => {
-            return new Http(backend, defaultOptions);
-          },
-          deps: [MockBackend, BaseRequestOptions],
-        },
       ]
     });
   });
 
-  it('should return the comments', inject([CommentsService, MockBackend], (service, backend) => {
-    backend.connections.subscribe(c => {
-      expect(c.request.url).toBe('http://fake/Plone/a-folder/test/@comments');
-      let response = {
-        "@id": "http://fake/Plone/a-folder/test/@comments",
-        "items": [
-          {
-            "@id": "http://fake/Plone/a-folder/test/@comments/1496662661977916",
-            "@parent": null,
-            "@type": "Discussion Item",
-            "author_name": "Bridgekeeper",
-            "author_username": "bkeeper",
-            "comment_id": "1496662661977916",
-            "creation_date": "2017-06-05T11:37:41",
-            "in_reply_to": null,
-            "modification_date": "2017-06-05T11:37:41",
-            "text": {
-              "data": "What... is your favourite colour?",
-              "mime-type": "text/plain"
-            },
-            "user_notification": null
+  it('should return the comments', () => {
+    const service = TestBed.get(CommentsService);
+    const http = TestBed.get(HttpTestingController);
+    let length = 0;
+    let author_name = '';
+
+    // fake response
+    const response = {
+      "@id": "http://fake/Plone/a-folder/test/@comments",
+      "items": [
+        {
+          "@id": "http://fake/Plone/a-folder/test/@comments/1496662661977916",
+          "@parent": null,
+          "@type": "Discussion Item",
+          "author_name": "Bridgekeeper",
+          "author_username": "bkeeper",
+          "comment_id": "1496662661977916",
+          "creation_date": "2017-06-05T11:37:41",
+          "in_reply_to": null,
+          "modification_date": "2017-06-05T11:37:41",
+          "text": {
+            "data": "What... is your favourite colour?",
+            "mime-type": "text/plain"
           },
-          {
-            "@id": "http://fake/Plone/a-folder/test/@comments/1496665801430054",
-            "@parent": null,
-            "@type": "Discussion Item",
-            "author_name": "Galahad of Camelot",
-            "author_username": "galahad",
-            "comment_id": "1496665801430054",
-            "creation_date": "2017-06-05T12:30:01",
-            "in_reply_to": null,
-            "modification_date": "2017-06-05T12:30:01",
-            "text": {
-              "data": "Blue. No, yel...\nauuuuuuuugh.",
-              "mime-type": "text/plain"
-            },
-            "user_notification": null
-          }
-        ],
-        "items_total": 2
-      };
-      c.mockRespond(new Response(new ResponseOptions({ body: response })));
-    });
+          "user_notification": null
+        },
+        {
+          "@id": "http://fake/Plone/a-folder/test/@comments/1496665801430054",
+          "@parent": null,
+          "@type": "Discussion Item",
+          "author_name": "Galahad of Camelot",
+          "author_username": "galahad",
+          "comment_id": "1496665801430054",
+          "creation_date": "2017-06-05T12:30:01",
+          "in_reply_to": null,
+          "modification_date": "2017-06-05T12:30:01",
+          "text": {
+            "data": "Blue. No, yel...\nauuuuuuuugh.",
+            "mime-type": "text/plain"
+          },
+          "user_notification": null
+        }
+      ],
+      "items_total": 2
+    };
+
     service.get('/a-folder/test').subscribe(res => {
       let comments = res.items;
-      expect(comments.length).toBe(2);
-      expect(comments[1].author_name).toBe('Galahad of Camelot');
+      length = comments.length;
+      author_name = comments[1].author_name;
     });
-  }));
+
+    http.expectOne('http://fake/Plone/a-folder/test/@comments').flush(response);
+
+    expect(length).toBe(2);
+    expect(author_name).toBe('Galahad of Camelot');
+  });
 
 });
