plone/plone.restapi-angular

commit 516f3d96a6a09e502a5bdcd0c4e4ff5bbf18d6de

Make local and global nav reactive

src/components/global.navigation.ts
@@ -1,37 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Services } from '../services';
 import { TraversingComponent } from '../traversing';
 import { NavLink } from '../interfaces';
 import { Target } from 'angular-traversal';
+import { Subscription } from 'rxjs/Subscription';
+import { ResourceService } from '../resource.service';
+import { Observable } from 'rxjs/Observable';
 
 
 @Component({
   selector: 'plone-global-navigation',
-  template: `<ul>
-  <li *ngFor="let link of links" [ngClass]="{'active': link.active}">
-    <a [traverseTo]="link.path">{{ link.title }}</a>
-  </li>
-</ul>`
+  template: `
+    <ul>
+      <li *ngFor="let link of links" [ngClass]="{'active': link.active}">
+        <a [traverseTo]="link.path">{{ link.title }}</a>
+      </li>
+    </ul>`
 })
-export class GlobalNavigation extends TraversingComponent implements OnInit {
+export class GlobalNavigation extends TraversingComponent implements OnInit, OnDestroy {
 
+  target: Target | null = null;
   links: NavLink[] = [];
+  navigationRefresh: Subscription;
 
-  constructor(
-    public services: Services,
-  ) {
+  constructor(public services: Services,) {
     super(services);
   }
 
   ngOnInit() {
     super.ngOnInit();
-    this.services.resource.navigation()
+    const component = this;
+
+    component.navigationRefresh = Observable.of(null)
+      .merge(component.services.navigation.refreshNavigation)
+      .mergeMap(() => component.services.resource.navigation())
       .subscribe((links: NavLink[]) => {
-        this.links = links;
-    });
+          component.links = links;
+          component.setActiveLinks();
+        }
+      )
+
   }
 
   onTraverse(target: Target) {
+    this.setActiveLinks();
+  }
+
+  ngOnDestroy() {
+    if (this.navigationRefresh.unsubscribe) {
+      this.navigationRefresh.unsubscribe();
+    }
+  }
+
+  private setActiveLinks() {
+    const target = this.target;
+    if (!target) {
+      return;
+    }
     this.links.map((link: NavLink) => {
       if (!target.path || target.path === '/') {
         link.active = (!link.path || link.path === '/');

src/components/breadcrumbs.spec.ts
@@ -3,7 +3,7 @@ import {
   HttpTestingController,
   HttpClientTestingModule
 } from '@angular/common/http/testing';
-import { Injectable, Directive, Input } from '@angular/core';
+import { Injectable, Directive, Input, EventEmitter } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { APP_BASE_HREF } from '@angular/common';
 import { Traverser, TraversalModule, Resolver, Marker, Normalizer, Target } from 'angular-traversal';
@@ -26,6 +26,8 @@ import { CacheService } from '../cache.service';
 
 @Injectable()
 class MockResourceService {
+
+  resourceModified = new EventEmitter();
   breadcrumbs(path: string) {
     return Observable.of([
       {

src/components/navigation.ts
@@ -1,31 +1,47 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { Services } from '../services';
 import { TraversingComponent } from '../traversing';
 import { NavTree } from '../interfaces';
 import { Target } from 'angular-traversal'
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'plone-navigation',
-  template: `<plone-navigation-level
+  template: `
+    <plone-navigation-level
       [links]="links"></plone-navigation-level>`
 })
-export class Navigation extends TraversingComponent {
+export class Navigation extends TraversingComponent implements OnDestroy {
 
   @Input() root: string = '/';
   @Input() depth: number = -1;
   links: NavTree[];
+  refreshNavigation: Subscription;
 
-  constructor(
-    public services: Services,
-  ) {
+  constructor(public services: Services,) {
     super(services);
   }
 
   onTraverse(target: Target) {
-    this.services.navigation
-      .getNavigationFor(target.context['@id'], this.root, this.depth)
+    const component = this;
+    const navigation = component.services.navigation;
+    component.removeSubscriptions();
+    this.refreshNavigation = Observable.of(null)
+      .merge(navigation.refreshNavigation)
+      .mergeMap(() => navigation.getNavigationFor(target.context['@id'], component.root, component.depth))
       .subscribe((tree: NavTree) => {
-        this.links = tree.children;
-    });
+        component.links = tree.children;
+      });
+  }
+
+  ngOnDestroy() {
+    this.removeSubscriptions();
+  }
+
+  private removeSubscriptions() {
+    if (this.refreshNavigation) {
+      this.refreshNavigation.unsubscribe()
+    }
   }
 }

src/navigation.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import 'rxjs/add/observable/of';
 import { Observable } from 'rxjs/Observable';
 import { ConfigurationService } from './configuration.service';
 import { NavTree } from './interfaces';
 
 import { ResourceService } from './resource.service';
+import { AuthenticationService } from './authentication.service';
 
 interface UnorderedContentTree {
   children: { [key: string]: UnorderedContentTree };
@@ -14,8 +15,16 @@ interface UnorderedContentTree {
 @Injectable()
 export class NavigationService {
 
-  constructor(private resource: ResourceService,
+  public refreshNavigation: EventEmitter<null> = new EventEmitter();
+
+  constructor(auth: AuthenticationService,
+              private resource: ResourceService,
               private config: ConfigurationService) {
+    resource.resourceModified
+      .merge(auth.isAuthenticated)
+      .subscribe(() => {
+        this.refreshNavigation.emit()
+      })
   }
 
   getNavigationFor(currentPath: string, root: string | number, depth: number): Observable<NavTree> {

tests/src/app/app.component.html
@@ -7,6 +7,7 @@
   <nav>
     <p>
       <a *ngIf="!logged" traverseTo="@@login">Log in</a>
+      <a *ngIf="logged" href="#" (click)="logout($event)">Log out</a>
     </p>
     <div>
       <h2>Custom global navigation</h2>

tests/src/app/app.component.ts
@@ -35,4 +35,9 @@ export class AppComponent {
       this.error = status.error ? status.error.message : '';
     });
   }
+
+  logout(event: Event) {
+    event.preventDefault();
+    this.services.authentication.logout();
+  }
 }

commit 71a08fb27bc75c535b09c4dc46fa62dfcc43aa6c

Fix missing active links in global navigation, bug introduced in 516f3d9. Target in global.navigation.ts -> setActiveLinks was always null, so setActiveLinks always returned with no effect. The pattern elsewhere in the code is to use the target in onTraverse only to set other state (not to keep it as part of the component state). So we use the same pattern here, passing target to setActiveLinks in onTraverse. setActiveLinks needs not to be called in ngOnInit, since ngOnInit of the parent class TraversingComponent triggers an onTraverse call when a target is ready (subscibing to target).

src/components/global.navigation.ts
@@ -19,7 +19,6 @@ import { Observable } from 'rxjs/Observable';
 })
 export class GlobalNavigation extends TraversingComponent implements OnInit, OnDestroy {
 
-  target: Target | null = null;
   links: NavLink[] = [];
   navigationRefresh: Subscription;
 
@@ -36,14 +35,13 @@ export class GlobalNavigation extends TraversingComponent implements OnInit, OnD
       .mergeMap(() => component.services.resource.navigation())
       .subscribe((links: NavLink[]) => {
           component.links = links;
-          component.setActiveLinks();
         }
       )
 
   }
 
   onTraverse(target: Target) {
-    this.setActiveLinks();
+    this.setActiveLinks(target);
   }
 
   ngOnDestroy() {
@@ -52,8 +50,7 @@ export class GlobalNavigation extends TraversingComponent implements OnInit, OnD
     }
   }
 
-  private setActiveLinks() {
-    const target = this.target;
+  private setActiveLinks(target: Target) {
     if (!target) {
       return;
     }