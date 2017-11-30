plone/plone.restapi-angular

commit 71a08fb27bc75c535b09c4dc46fa62dfcc43aa6c

Fix missing active links in global navigation, bug introduced in 516f3d9. Target in global.navigation.ts -> setActiveLinks was always null, so setActiveLinks always returned with no effect. The pattern elsewhere in the code is to use the target in onTraverse only to set other state (not to keep it as part of the component state). So we use the same pattern here, passing target to setActiveLinks in onTraverse. setActiveLinks needs not to be called in ngOnInit, since ngOnInit of the parent class TraversingComponent triggers an onTraverse call when a target is ready (subscibing to target).

src/components/global.navigation.ts
@@ -19,7 +19,6 @@ import { Observable } from 'rxjs/Observable';
 })
 export class GlobalNavigation extends TraversingComponent implements OnInit, OnDestroy {
 
-  target: Target | null = null;
   links: NavLink[] = [];
   navigationRefresh: Subscription;
 
@@ -36,14 +35,13 @@ export class GlobalNavigation extends TraversingComponent implements OnInit, OnD
       .mergeMap(() => component.services.resource.navigation())
       .subscribe((links: NavLink[]) => {
           component.links = links;
-          component.setActiveLinks();
         }
       )
 
   }
 
   onTraverse(target: Target) {
-    this.setActiveLinks();
+    this.setActiveLinks(target);
   }
 
   ngOnDestroy() {
@@ -52,8 +50,7 @@ export class GlobalNavigation extends TraversingComponent implements OnInit, OnD
     }
   }
 
-  private setActiveLinks() {
-    const target = this.target;
+  private setActiveLinks(target: Target) {
     if (!target) {
       return;
     }

commit 1e0a77868f0b9006ffb5085cfebcc747ba6cfde6

call setActiveLinks when refreshing navigation (bug introduced in 71a08fb); move link handling to overridable method

src/components/global.navigation.ts
@@ -20,6 +20,7 @@ export class GlobalNavigation extends TraversingComponent implements OnInit, OnD
 
   links: NavLink[] = [];
   refreshNavigation: Subscription;
+  contextPath: string;
 
   constructor(public services: Services) {
     super(services);
@@ -33,14 +34,19 @@ export class GlobalNavigation extends TraversingComponent implements OnInit, OnD
       .merge(component.services.navigation.refreshNavigation)
       .mergeMap(() => component.services.resource.navigation())
       .subscribe((links: NavLink[]) => {
-          component.links = links;
-        }
-      );
+        this.setLinks(links);
+      });
+  }
 
+  public setLinks(links: NavLink[]) {
+    this.links = links;
+    this.setActiveLinks(this.contextPath);
   }
 
   onTraverse(target: Target) {
-    this.setActiveLinks(target);
+    // contextPath = '' for the root of the site - always set the contextPath
+    this.contextPath = target.contextPath;
+    this.setActiveLinks(this.contextPath);
   }
 
   ngOnDestroy() {
@@ -49,15 +55,12 @@ export class GlobalNavigation extends TraversingComponent implements OnInit, OnD
     }
   }
 
-  private setActiveLinks(target: Target) {
-    if (!target) {
-      return;
-    }
+  private setActiveLinks(contextPath: string) {
     this.links.map((link: NavLink) => {
-      if (!target.path || target.path === '/') {
+      if (!contextPath || contextPath === '/') {
         link.active = (!link.path || link.path === '/');
       } else {
-        let targetList: Array<String> = target.path.split('/');
+        let targetList: Array<String> = contextPath.split('/');
         let linkList: Array<String> = link.path.split('/');
         let isSubpath = true;   // you could just use link.active
         for (const {item, index} of linkList.map((item, index) => ({ item, index }))) {